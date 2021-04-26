e0246875e3a404f2350dc3bbee0b3e26f3cc6295
anovotarsk <novotarskiy30112000@gmail.com>
2021-04-26 20:37:01 +0300

    add .gitignore

--- build/libkeepass/src/CMakeFiles/keepasslib_autogen.dir/cmake_clean.cmake
+++
@@ -1,9 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/keepasslib_autogen"
-  "keepasslib_autogen/mocs_compilation.cpp"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/keepasslib_autogen.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
